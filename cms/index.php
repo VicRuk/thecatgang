47a80c9739ccef0797a3af6b26bf72ff6db9d811
Yamas <matheusyamas@gmail.com>
2024-06-06 23:29:43 -0300

    v0.6

---
+++ cms/index.php
@@ -0,0 +1,36 @@
+<?php
+include("../models/conexao.php");
+include("../views/blades/header2.php");
+?>
+<div class="body-admin">
+<div class="container d-flex justify-content-center align-items-center min-vh-100">
+    <div class="row border rounded-5 p-3 bg-white shadow box-area">
+        <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background:#FFCB04 ;">
+            <div class="featured-image mb-3">
+                <img src="../files/images/logo.png" class="img-fluid" style="width: 500px">
+            </div>
+            <p class="text fs-3">The Cat Gang</p>
+        </div>
+        <div class="col-md-6 right-box">
+            <div class="row align-items-center">
+                <div class="header-text mb-4">
+                    <h2>Bem Vindo, Admin</h2>
+                    <p>coloque seus dados abaixo:</p>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Usuario">
+                </div>
+                <div class="input-group mb-1">
+                    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Senha">
+                </div>
+                <div class="input-group mb-3 botao-login" >
+                    <button class="btn btn-lg btn-warning w-100 fs-6">Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<?php
+include("../views/blades/footer2.php");
+?>
---
+++ views/blades/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+        
+    <!-- Ícones -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="js/funcoes.js" type="text/javascript"></script>
+    
+    <title>The Cat Gang</title>
+</head>
+<body>
+    <div class="container-fluid px-0 bg-white">
+        <!-- NAV -->
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light px-3 ">
+            <div class="container-fluid" id="nav-content">
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <img src="files/images/logo.png" class="col-3">
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body d-flex flex-column flex-lg-row align-items-lg-center">
+                        <a href="#" class="logo-link"><img src="files/images/logo.png" class="img-fluid" style="width: 100px !important;"></a>
+                        <ul class="navbar-nav d-flex flex-column flex-lg-row justify-content-center flex-grow-1">
+                            <li class="nav-item px-1">
+                                <a class="fw-semibold nav-link active text-dark" aria-current="page" href="#sobre">Sobre</a>
+                            </li>
+                            <li class="nav-item px-1">
+                                <a class="fw-semibold nav-link active text-dark" aria-current="page" href="#projeto">Projeto</a>
+                            </li>
+                            <li class="nav-item px-1">
+                                <a class="fw-semibold nav-link active text-dark" aria-current="page" href="#equipe">Equipe</a>
+                            </li>
+                            <li class="nav-item px-1">
+                                <a class="fw-semibold nav-link active text-dark" aria-current="page" href="#home-noticias">Notícias</a>
+                            </li>
+                        </ul>
+                        <a class="nav-link active text-dark" aria-current="page" href="views/login.php">
+                            Log in
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
